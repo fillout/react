0c490fdc891c99de4f76a7f9cee6d264c911fcda
Archie Baer <archie@abaer.dev>
2023-10-18 18:54:02 +0100

    FullScreen

---
+++ src/embeds/FullScreen.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { FormParams, useFilloutEmbed } from "../embed.js";
+import { Loading } from "../components/Loading.js";
+
+type FullScreenProps = {
+  filloutId: string;
+  inheritParameters?: boolean;
+  parameters?: FormParams;
+};
+
+export const FullScreen = ({
+  filloutId,
+  inheritParameters,
+  parameters,
+}: FullScreenProps) => {
+  const embed = useFilloutEmbed({
+    filloutId,
+    inheritParameters,
+    parameters,
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      {loading && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <Loading />
+        </div>
+      )}
+
+      {embed && (
+        <iframe
+          src={embed.iframeUrl}
+          allow="microphone; camera; geolocation"
+          title="Embedded Form"
+          style={{
+            border: 0,
+            width: !loading ? "100%" : 0,
+            height: !loading ? "100%" : 0,
+            opacity: !loading ? 1 : 0,
+          }}
+          onLoad={() => setLoading(false)}
+        />
+      )}
+    </div>
+  );
+};
