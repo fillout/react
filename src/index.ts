d337900f2b7a1de2892076893eb0accb37e496ac
Archie Baer <archie@abaer.dev>
2023-10-18 23:19:31 +0100

    export all embed types from index file

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Standard as FilloutStandardEmbed } from "./embeds/Standard.js";
+import { FullScreen as FilloutFullScreenEmbed } from "./embeds/FullScreen.js";
+import { Popup as FilloutPopupEmbed } from "./embeds/Popup.js";
+import { PopupButton as FilloutPopupEmbedButton } from "./embeds/PopupButton.js";
+import { Slider as FilloutSliderEmbed } from "./embeds/Slider.js";
+import { SliderButton as FilloutSliderEmbedButton } from "./embeds/SliderButton.js";
+
+export {
+  FilloutStandardEmbed,
+  FilloutFullScreenEmbed,
+  FilloutPopupEmbed,
+  FilloutPopupEmbedButton,
+  FilloutSliderEmbed,
+  FilloutSliderEmbedButton,
+};
