b2ca63c21a0ee2683a5c4f8eaa2abeb6aa8386f1
Archie Baer <archie@abaer.dev>
2023-10-21 21:48:10 +0100

    better index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import { Standard as FilloutStandardEmbed } from "./embeds/Standard.js";
-import { FullScreen as FilloutFullScreenEmbed } from "./embeds/FullScreen.js";
-import { Popup as FilloutPopupEmbed } from "./embeds/Popup.js";
-import { PopupButton as FilloutPopupEmbedButton } from "./embeds/PopupButton.js";
-import { Slider as FilloutSliderEmbed } from "./embeds/Slider.js";
-import { SliderButton as FilloutSliderEmbedButton } from "./embeds/SliderButton.js";
-
-export {
-  FilloutStandardEmbed,
-  FilloutFullScreenEmbed,
-  FilloutPopupEmbed,
-  FilloutPopupEmbedButton,
-  FilloutSliderEmbed,
-  FilloutSliderEmbedButton,
-};
+export { Standard as FilloutStandardEmbed } from "./embeds/Standard.js";
+export { FullScreen as FilloutFullScreenEmbed } from "./embeds/FullScreen.js";
+export { Popup as FilloutPopupEmbed } from "./embeds/Popup.js";
+export { PopupButton as FilloutPopupEmbedButton } from "./embeds/PopupButton.js";
+export { Slider as FilloutSliderEmbed } from "./embeds/Slider.js";
+export { SliderButton as FilloutSliderEmbedButton } from "./embeds/SliderButton.js";
