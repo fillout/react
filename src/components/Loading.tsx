154f8dc60350c4fcb25d4e574325bb7eae9d8358
xkcdstickfigure <97917457+xkcdstickfigure@users.noreply.github.com>
2025-03-24 04:47:01 +0000

    spinner

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,41 +1,14 @@
 import React from "react";
 
 export const Loading = () => (
-  <div>
-    <div
-      style={{
-        color: "white",
-        fontSize: 24,
-        lineHeight: "24px",
-        fontWeight: 600,
-        marginBottom: 16,
-        textAlign: "center",
-        fontFamily: "sans-serif",
-      }}
-    >
-      Loading
-    </div>
-
-    <div
-      style={{
-        height: 20,
-        width: 150,
-        display: "flex",
-        position: "relative",
-        alignItems: "center",
-        justifyContent: "start",
-      }}
-    >
-      <div
-        style={{
-          boxShadow: "0px 5px 40px -1px #ffffffcc",
-          borderRadius: 10,
-          background: "white",
-          height: 7,
-          width: 0,
-          animation: "fillout-embed-loading 1s ease forwards",
-        }}
-      />
-    </div>
-  </div>
+  <div
+    style={{
+      border: "solid 2px #aaa",
+      borderBottomColor: "white",
+      borderRadius: "50%",
+      width: 32,
+      height: 32,
+      animation: "fillout-embed-loading 1s infinite linear",
+    }}
+  ></div>
 );
